5459392f538050811fbcb0ffecd8843ca1d8a8aa
gustavohubner <34004020+gustavohubner@users.noreply.github.com>
2020-11-18 14:25:58 -0300

    a

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.io.File;
 public class Main {
 
     public static void main(String[] args) {
-        File file = new File("input/fatorial.txt");
+        File file = new File("input/fatorial-contando-n-1.txt");
         // Rodar via console
         /* 
         if (file.exists()) {
