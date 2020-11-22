267fbd018d05df8c808cc150d5c6f7898e92b9b4
William Kolodziejski <wkolodziejski@inf.ufpel.edu.br>
2020-11-22 18:44:44 -0300

    binaries cleanup

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@ import java.io.File;
 public class Main {
 
     public static void main(String[] args) {
-        File file = new File("input/fatorial.txt");
+        File file = new File("input/fatorial");
 
         Interface f = new Interface(Assembler.convert(file, 10));
         f.setVisible(true);
     }
+
 }
